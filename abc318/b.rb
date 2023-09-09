677c37fe4b210a4d4f1f55e6aae070e88ef39801
yoto1980yen <yotolemontea0725@yahoo.co.jp>
2023-09-09 20:41:06 +0900

    add abc318

---
+++ abc318/b.rb
@@ -0,0 +1,131 @@
+def main
+    n = int
+    map = make_map(n, "i")
+    m = []
+    101.times do |i|
+        m << Array.new(101, 0)
+    end
+    map.each do |i|
+        a, b, c, d = i
+        (a...b).each do |x|
+            (c...d).each do |y|
+                m[y][x] += 1
+            end
+        end
+    end
+    ans = 0
+    m.each do |i|
+        i.each do |j|
+            ans += 1 if j >= 1
+        end
+    end
+    puts ans
+end
+
+#----------------------------------------------------------------------------------
+require "set"
+def int
+    gets.to_i
+end
+
+def intary
+    gets.split(" ").map(&:to_i)
+end
+
+def str
+    gets.chomp
+end
+
+def strary
+    gets.chomp.split("")
+end
+
+def is_lower?(c)
+    c != c.upcase
+end
+
+def grouping(ary)
+    ary.slice_when { |a, b| a != b }.to_a
+end
+
+def number?(str)
+nil != (str =~ /\A[0-9]+\z/)
+end
+
+def make_tree(n, path, way = false)
+    if n != 0
+        tree = {}
+        path.times do |i|
+            u,v = intary
+            tree[u] ? tree[u] << v : tree[u] = v
+            if way
+                tree[v] ? tree[v] << u : tree[v] = u
+            end
+        end
+    else
+        tree = Array.new(n+1) {Array.new}
+        path.times do |i|
+            u,v = intary
+            tree[u] << v
+            if way
+                tree[v] << u
+            end
+        end
+    end
+    return tree
+end
+
+def make_visited(n)
+    Array.new(n+1, false)
+end
+
+def make_map(n, type)
+    map = []
+    if type == "s"
+        n.times do |i|
+            map << strary
+        end
+    elsif type == "i"
+        n.times do |i|
+            map << intary
+        end
+    end
+    return map
+end
+
+def nisinsu(int)
+    return int.to_s(2).chomp
+end
+
+def zeroume(keta, int)
+    sprintf("%0#{keta}d", int)
+end
+
+# 約数列挙
+def divisors(n)
+    result = []
+    
+    doit = ->(pd, acc) {
+        return if pd.empty?
+        x, *xs = pd
+        (0..x[1]).each do |i|
+            e = acc * x[0] ** i
+            result << e
+            doit.(xs, e)
+        end
+    }
+    doit.(n.prime_division, 1)
+    
+    result.uniq.sort
+end
+# 累積和作成
+def ruiseki(arr)
+    s = Array.new(arr.size + 1)
+    s[0] = 0
+    (0...arr.size).each do |i|
+        s[i+1] = s[i] + arr[i]
+    end
+    return s
+end
+
+main
---
+++ abc318/c.rb
@@ -0,0 +1,150 @@
+def main
+    n, d, p = intary
+    f = intary.sort.reverse
+    syuukai = p / d
+    count = 0
+    cache = 0
+    ans = 0
+    f.each.with_index do |v, i|
+        if v >= syuukai
+            count += 1
+            cache += v
+            if count == d
+                ans += p 
+                count = 0
+                cache = 0
+            end
+            next
+        end
+        if count != 0
+            cache += v
+            count += 1
+            if count == d
+                if cache >= p
+                    ans += p
+                else
+                    ans += cache
+                end
+                count = 0
+            end
+            next
+        end
+        ans += v
+    end
+    if count != 0
+        if cache >= p
+            ans += p
+        else
+            ans += cache
+        end
+    end
+    pp ans
+end
+
+#----------------------------------------------------------------------------------
+require "set"
+def int
+    gets.to_i
+end
+
+def intary
+    gets.split(" ").map(&:to_i)
+end
+
+def str
+    gets.chomp
+end
+
+def strary
+    gets.chomp.split("")
+end
+
+def is_lower?(c)
+    c != c.upcase
+end
+
+def grouping(ary)
+    ary.slice_when { |a, b| a != b }.to_a
+end
+
+def number?(str)
+nil != (str =~ /\A[0-9]+\z/)
+end
+
+def make_tree(n, path, way = false)
+    if n != 0
+        tree = {}
+        path.times do |i|
+            u,v = intary
+            tree[u] ? tree[u] << v : tree[u] = v
+            if way
+                tree[v] ? tree[v] << u : tree[v] = u
+            end
+        end
+    else
+        tree = Array.new(n+1) {Array.new}
+        path.times do |i|
+            u,v = intary
+            tree[u] << v
+            if way
+                tree[v] << u
+            end
+        end
+    end
+    return tree
+end
+
+def make_visited(n)
+    Array.new(n+1, false)
+end
+
+def make_map(n, type)
+    map = []
+    if type == "s"
+        n.times do |i|
+            map << strary
+        end
+    elsif type == "i"
+        n.times do |i|
+            map << intary
+        end
+    end
+    return map
+end
+
+def nisinsu(int)
+    return int.to_s(2).chomp
+end
+
+def zeroume(keta, int)
+    sprintf("%0#{keta}d", int)
+end
+
+# 約数列挙
+def divisors(n)
+    result = []
+    
+    doit = ->(pd, acc) {
+        return if pd.empty?
+        x, *xs = pd
+        (0..x[1]).each do |i|
+            e = acc * x[0] ** i
+            result << e
+            doit.(xs, e)
+        end
+    }
+    doit.(n.prime_division, 1)
+    
+    result.uniq.sort
+end
+# 累積和作成
+def ruiseki(arr)
+    s = Array.new(arr.size + 1)
+    s[0] = 0
+    (0...arr.size).each do |i|
+        s[i+1] = s[i] + arr[i]
+    end
+    return s
+end
+
+main
