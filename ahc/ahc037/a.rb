d073ed6666ac7287b46801e05acbbc7bfcb3ca63
yoto1980yen <yotolemontealove0725@yahoo.co.jp>
2024-09-21 20:52:27 +0900

    add: ahc037

---
+++ ahc/ahc037/a.rb
@@ -0,0 +1,383 @@
+def main
+    n = int
+    map = []
+    n.times do |i|
+        map << intary
+    end
+    # i[0]基準バージョン
+    costmax = 0
+    mapset = {0 => RBTree.new}
+    mapset[0][0] = true
+    ans = []
+    sweet = [0]
+    sakujo = []
+    map.sort!.each.with_index do |i, j|
+        next if i[0] == 0
+        if sweet.last != i[0]
+            ans << [sweet.last, 0, i[0], 0]
+            unless mapset[i[0]]
+                mapset[i[0]] = RBTree.new
+                mapset[i[0]][0] = true
+            end
+            sakujo << j if i[1] == 0
+            sweet << i[0]
+
+        end
+    end
+    soda = []
+    map.each.with_index do |i, j|
+        if sakujo.first == j
+            sakujo.shift
+            next
+        end
+        soda << i
+    end
+    soda.each.with_index do |i, v|
+        now = [0,0,i[0],i[1]]
+        cost = 1000000000000
+        search_index = sweet.bsearch_index{ |k| k >= i[0] }
+        (0...(search_index+1)).each do |j|
+            nowcost = i[0] - sweet[j] + i[1] - mapset[sweet[j]].upper_bound(i[1])[0]
+            if cost >= nowcost
+                now = [sweet[j], mapset[sweet[j]].upper_bound(i[1])[0], i[0], i[1]]
+                cost = nowcost
+            end
+        end
+        costmax += cost
+        ans << now
+        mapset[now[2]][now[3]] = true
+    end
+    # i[1] version
+    costmaxx = 0
+    mapset = {0 => RBTree.new}
+    mapset[0][0] = true
+    anss = []
+    sweet = [0]
+    sakujo = []
+    map.sort{|a,b| a[1] <=> b[1]}.each.with_index do |i, j|
+        next if i[1] == 0
+        if sweet.last != i[1]
+            anss << [0, sweet.last, 0, i[1]]
+            unless mapset[i[1]]
+                mapset[i[1]] = RBTree.new
+                mapset[i[1]][0] = true
+            end
+            sakujo << j if i[1] == 0
+            sweet << i[1]
+
+        end
+    end
+    soda = []
+    map.each.with_index do |i, j|
+        if sakujo.first == j
+            sakujo.shift
+            next
+        end
+        soda << i
+    end
+    soda.each.with_index do |i, v|
+        now = [0,0,i[0],i[1]]
+        cost = 1000000000000
+        search_index = sweet.bsearch_index{ |k| k >= i[1] }
+        (0...(search_index+1)).each do |j|
+            nowcost = i[0] - mapset[sweet[j]].upper_bound(i[0])[0] + i[1] - sweet[j]
+            if cost >= nowcost
+                now = [mapset[sweet[j]].upper_bound(i[0])[0], sweet[j], i[0], i[1]]
+                cost = nowcost
+            end
+        end
+        costmaxx += cost
+        anss << now
+        mapset[now[3]][now[2]] = true
+    end
+    if costmaxx < costmax
+        puts anss.size
+        anss.each do |i|
+            puts i.join(" ")
+        end
+    else
+        puts ans.size
+        ans.each do |i|
+            puts i.join(" ")
+        end
+    end
+end
+
+#----------------------------------------------------------------------------------
+require "set"
+require "rbtree"
+def int
+    gets.chomp.to_i
+end
+
+def intary
+    gets.chomp.split(" ").map(&:to_i)
+end
+
+def str
+    gets.chomp
+end
+
+def strary
+    gets.chomp.split("")
+end
+
+def is_lower?(c)
+    c != c.upcase
+end
+
+def grouping(ary)
+    ary.slice_when { |a, b| a != b }.to_a
+end
+
+def number?(str)
+nil != (str =~ /\A[0-9]+\z/)
+end
+
+# 階乗
+def factorial(n, bottom=1)
+    n == 0 ? 1 : (bottom..n).inject(:*)
+end
+
+# nCr
+def nCr(n, r)
+    factorial(n) / (factorial(r) * factorial(n-r))
+end
+
+def make_tree(n, path, way = false)
+    if n != 0
+        tree = {}
+        path.times do |i|
+            u,v = intary
+            tree[u] ? tree[u] << v : tree[u] = v
+            if way
+                tree[v] ? tree[v] << u : tree[v] = u
+            end
+        end
+    else
+        tree = Array.new(n+1) {Array.new}
+        path.times do |i|
+            u,v = intary
+            tree[u] << v
+            if way
+                tree[v] << u
+            end
+        end
+    end
+    return tree
+end
+
+def make_visited(n)
+    Array.new(n+1, false)
+end
+
+def make_map(n, type)
+    map = []
+    if type == "s"
+        n.times do |i|
+            map << strary
+        end
+    elsif type == "i"
+        n.times do |i|
+            map << intary
+        end
+    end
+    return map
+end
+
+def nisinsu(int)
+    return int.to_s(2).chomp
+end
+
+def zeroume(keta, int)
+    sprintf("%0#{keta}d", int)
+end
+
+# 約数列挙
+def divisors(n)
+    result = []
+    
+    doit = ->(pd, acc) {
+        return if pd.empty?
+        x, *xs = pd
+        (0..x[1]).each do |i|
+            e = acc * x[0] ** i
+            result << e
+            doit.(xs, e)
+        end
+    }
+    doit.(n.prime_division, 1)
+    
+    result.uniq.sort
+end
+# 累積和作成
+def ruiseki(arr)
+    s = Array.new(arr.size + 1)
+    s[0] = 0
+    (0...arr.size).each do |i|
+        s[i+1] = s[i] + arr[i]
+    end
+    return s
+end
+class UnionFind
+    def initialize(size)
+        @rank = Array.new(size, 0)
+        @parent = Array.new(size, &:itself)
+    end
+
+    def unite(id_x, id_y)
+        x_parent = get_parent(id_x)
+        y_parent = get_parent(id_y)
+        return if x_parent == y_parent
+        if @rank[x_parent] > @rank[y_parent]
+            @parent[y_parent] = x_parent
+        else
+            @parent[x_parent] = y_parent
+            @rank[y_parent] += 1 if @rank[x_parent] == @rank[y_parent]
+        end
+    end
+
+    def get_parent(id_x)
+        @parent[id_x] == id_x ? id_x : (@parent[id_x] = get_parent(@parent[id_x]))
+    end
+
+    def same_parent?(id_x, id_y)
+        get_parent(id_x) == get_parent(id_y)
+    end
+    def size
+        @parent.map { |id_x| get_parent(id_x) }.uniq.size
+        # @parent.map.with_index.count(&:==) 
+        # とすることも出来ます!
+    end
+end
+
+class PriorityQueue
+    # By default, the priority queue returns the maximum element first.
+    # If a block is given, the priority between the elements is determined with it.
+    # For example, the following block is given, the priority queue returns the minimum element first.
+    # `PriorityQueue.new { |x, y| x < y }`
+    #
+    # A heap is an array for which a[k] <= a[2*k+1] and a[k] <= a[2*k+2] for all k, counting elements from 0.
+    def initialize(array = [], &comp)
+        @heap = array
+        @comp = comp || proc { |x, y| x > y }
+        heapify
+    end
+
+    attr_reader :heap
+
+    # Push new element to the heap.
+    def push(item)
+    shift_down(0, @heap.push(item).size - 1)
+    end
+
+    alias << push
+    alias append push
+
+    # Pop the element with the highest priority.
+    def pop
+        latest = @heap.pop
+        return latest if empty?
+
+        ret_item = heap[0]
+        heap[0] = latest
+        shift_up(0)
+        ret_item
+    end
+
+    # Get the element with the highest priority.
+    def get
+        @heap[0]
+    end
+
+    alias top get
+
+    # Returns true if the heap is empty.
+    def empty?
+        @heap.empty?
+    end
+
+    private
+
+    def heapify
+        (@heap.size / 2 - 1).downto(0) { |i| shift_up(i) }
+    end
+
+    def shift_up(pos)
+        end_pos = @heap.size
+        start_pos = pos
+        new_item = @heap[pos]
+        left_child_pos = 2 * pos + 1
+    
+        while left_child_pos < end_pos
+            right_child_pos = left_child_pos + 1
+            if right_child_pos < end_pos && @comp.call(@heap[right_child_pos], @heap[left_child_pos])
+                left_child_pos = right_child_pos
+            end
+            # Move the higher priority child up.
+            @heap[pos] = @heap[left_child_pos]
+            pos = left_child_pos
+            left_child_pos = 2 * pos + 1
+        end
+        @heap[pos] = new_item
+        shift_down(start_pos, pos)
+    end
+
+    def shift_down(star_pos, pos)
+        new_item = @heap[pos]
+        while pos > star_pos
+            parent_pos = (pos - 1) >> 1
+            parent = @heap[parent_pos]
+            break if @comp.call(parent, new_item)
+
+            @heap[pos] = parent
+            pos = parent_pos
+        end
+        @heap[pos] = new_item
+    end
+end
+# エラトステネスの篩で素数列挙して高速に素因数分解したい！
+# ref: https://atcoder.jp/contests/abc177/editorial/82
+class PrimeDivWithSieve
+    def initialize(n)
+        @sieve = [] # nまでの素数を入れる
+        @min_div = {} # keyの値の最小の素因数を入れる
+        # 他を篩落とし得る素数はsqrtを上限にできる
+        (2..Math.sqrt(n).floor).each do |i|
+            next if @min_div[i] # ここに値が入ってる = ふるい落とされている
+            @sieve << i # ふるい落とされずに来たらそいつは素数
+    
+            sieve_target = i * i
+            while sieve_target <= n do
+            @min_div[sieve_target] ||= i
+            sieve_target += i
+            end
+        end
+        (Math.sqrt(n).floor.next..n).each do |i|
+            next if @min_div[i]
+            @sieve << i
+        end
+    end
+    # Integer#prime_division と同じ値を返すようにする
+    # https://docs.ruby-lang.org/ja/latest/method/Integer/i/prime_division.html
+    def prime_division(num)
+        return num if !@min_div[num] # 素数のときすぐ返す
+        return_num = 1 # [[a, x], [b, y]] <=> num = a^x * b^y
+        while num > 1 do
+            prime = @min_div[num] # 最小の素因数, nil => numが素数
+            break return_num *= num if !prime
+            div_total = 0
+            while num % prime == 0 do
+            num /= prime
+            div_total += 1
+            end
+            return_num *= prime  if div_total % 2 != 0
+        end
+        return_num
+    end
+    def prime_list
+        @sieve
+    end
+end
+HeapQueue = PriorityQueue
+main
